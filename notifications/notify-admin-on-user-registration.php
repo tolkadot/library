e088d311f7bb0e6987096a8b48082375457978a2
Christophe Battarel <christophe.battarel@altairis.fr>
2015-11-11 15:52:30 +0100

    add charitable prefix to function name

--- notifications/notify-admin-on-user-registration.php
+++ notifications/notify-admin-on-user-registration.php
@@ -7,9 +7,9 @@
  * @return  array[]
  */
 
-function notify_new_user($user_id, $values)
+function charitable_notify_new_user($user_id, $values)
 {
 	wp_new_user_notification( $user_id, null, 'admin' );
 }
 
-add_action('charitable_after_insert_user', 'notify_new_user', 10, 2);
+add_action('charitable_after_insert_user', 'charitable_notify_new_user', 10, 2);

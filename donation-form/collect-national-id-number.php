f7c35a8b40dacbe7d3be814c632b407f8efca30a
Eric Daams <eric@164a.com>
2016-02-03 10:39:24 +0930

    Added function to custom donation field example, showing how to also display the value in the donation admin

--- donation-form/collect-national-id-number.php
+++ donation-form/collect-national-id-number.php
@@ -20,3 +20,23 @@ function ed_collect_national_id_number( $fields, Charitable_Donation_Form $form 
 }
 
 add_filter( 'charitable_donation_form_user_fields', 'ed_collect_national_id_number', 10, 2 );
+
+/**
+ * Display the National ID # in the admin donation details box.
+ *
+ * @param   array[] $meta
+ * @param   Charitable_Donation $donation
+ * @return  array[]
+ */
+function ed_show_national_id_number_in_admin( $meta, $donation ) {
+    $donor_data = $donation->get_donor_data();
+
+    $meta[ 'national_id_number' ] = array(
+        'label'     => __( 'National ID Number', 'your-namespace' ),
+        'value'     => $donor_data[ 'national_id_number' ]
+    );
+
+    return $meta;
+}
+
+add_filter( 'charitable_donation_admin_meta', 'ed_show_national_id_number_in_admin', 10, 2 );

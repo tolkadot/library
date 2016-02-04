3d155b8bfa716e814c8d028966bbcfbe501e8e35
Eric Daams <eric@164a.com>
2016-02-05 08:46:10 +0930

    Came up with a better way to auto approve campaigns.

--- extensions/ambassadors/campaign-submission-form/auto-approve-campaigns.php
+++ extensions/ambassadors/campaign-submission-form/auto-approve-campaigns.php
@@ -1,9 +1,24 @@
 <?php 
 /**
- * Automatically set campaigns as published when they are submitted. 
+ * Set campaigns to published after they are submitted.
+ * 
+ * @param   array $submitted
+ * @param   int $campaign_id     
  */
-function ed_auto_approve_campaigns( $status ) {
-    return 'publish';
+function ed_auto_approve_campaigns( $submitted, $campaign_id ) {
+    /**
+     * Check whether the campaign is pending. Don't touch 
+     * campaigns that are being previewed or have already 
+     * been published.
+     */
+    if ( 'pending' == get_post_status( $campaign_id ) ) {
+
+        wp_update_post( array(
+            'ID' => $campaign_id,
+            'post_status' => 'publish'
+        ) );
+
+    }
 }
 
-add_filter( 'charitable_campaign_submission_initial_status', 'ed_auto_approve_campaigns' );
+add_action( 'charitable_campaign_submission_save', 'ed_auto_approve_campaigns', 10, 2 );

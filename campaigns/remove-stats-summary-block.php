f0449987757eb5461b55f052f7322c411785b4cc
Eric Daams <eric@164a.com>
2016-02-18 19:46:37 +0930

    Modified the code snippet shoing how to remove the stats summary, with an option to add the donate button back in.

--- campaigns/remove-stats-summary-block.php
+++ campaigns/remove-stats-summary-block.php
@@ -4,6 +4,9 @@
  */
 function en_remove_campaign_summary_block() {
     remove_action( 'charitable_campaign_content_before', 'charitable_template_campaign_summary', 6 );
+
+    // If you still want to show a Donate button, uncomment the line below.
+    // add_action( 'charitable_campaign_content_before', 'charitable_template_donate_button', 6 );
 }
 
 add_action( 'after_setup_theme', 'en_remove_campaign_summary_block', 11 );

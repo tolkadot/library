6b08bf5b1c8ec263e2950b836fa63d1e05850dcb
Eric Daams <eric@164a.com>
2017-08-07 10:53:32 +0930

    Get campaign end date formatted, correctly.

--- extensions/ambassadors/campaign-submission-form/replace-campaign-length-with-end-date-field.php
+++ extensions/ambassadors/campaign-submission-form/replace-campaign-length-with-end-date-field.php
@@ -6,23 +6,23 @@
  * set up out of the box. You need to use jQuery's Datepicker to add the 
  * datepicker UI.
  */
-function ed_replace_campaign_length_with_end_date_field( $fields, $form ) {    
+function ed_charitable_replace_campaign_length_with_end_date_field( $fields, $form ) {    
     unset( $fields[ 'length' ] );
 
     $fields[ 'end_date' ] = array(
-        'label'         => __( 'End Date', 'pp-toolkit' ), 
+        'label'         => __( 'End Date', 'ed-charitable' ), 
         'type'          => 'datepicker',
         'priority'      => 8, 
         'required'      => true, 
         'data_type'     => 'core',
-        'editable'      => false
+        'editable'      => false,
     );
 
     if ( $form->get_campaign() ) {
-        $fields[ 'end_date' ][ 'value' ] = date( 'm/d/Y', strtotime( $form->get_campaign()->get( 'end_date' ) ) );
-    }
+        $fields[ 'end_date' ][ 'value' ] = $form->get_campaign()->get_end_date( 'd/m/Y' );
+    }    
 
     return $fields;
 }
 
-add_filter( 'charitable_campaign_submission_campaign_fields', 'ed_replace_campaign_length_with_end_date_field', 10, 2 );
+add_filter( 'charitable_campaign_submission_campaign_fields', 'ed_charitable_replace_campaign_length_with_end_date_field', 10, 2 );

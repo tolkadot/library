56e985e2ddd6e0e1f694bdd6a9d817f3b38dd510
Eric Daams <eric@164a.com>
2018-02-07 14:22:37 +0930

    Swapped snippets

--- donation-form/legacy/set-minimum-donation-amount.php
+++ donation-form/legacy/set-minimum-donation-amount.php
@@ -2,12 +2,31 @@
 /**
  * Set the minimum donation amount required.
  *
- * This requires Charitable 1.5+. If you are using a previous version:
+ * This function is called when the donation amount is validated, so it needs
+ * to return either true or false.
  *
- * @see https://github.com/Charitable/library/blob/master/donation-form/legacy/set-minimum-donation-amount.php
+ * Note that as of Charitable 1.5, there is a better way to do this:
+ *
+ * @see https://github.com/Charitable/library/blob/master/donation-form/set-minimum-donation-amount.php
  */
-function ed_charitable_set_minimum_donation_amount() {
-    return 2;
+function ed_set_minimum_donation_amount( $valid, Charitable_Donation_Form_Interface $donation_form ) {
+
+    /* Replace 2 with your minimum donation amount. */
+    $minimum = 2;
+
+    if ( $donation_form::get_donation_amount() < $minimum ) {
+
+        charitable_get_notices()->add_error( sprintf(
+            __( 'You must donate more than %s.', 'charitable' ),
+            charitable_format_money( $minimum )
+        ) );    
+
+        $valid = false;
+
+    }
+
+    return $valid;
+
 }
 
-add_filter( 'charitable_minimum_donation_amount', 'ed_charitable_set_minimum_donation_amount' );
+add_filter( 'charitable_validate_donation_form_submission_amount_check', 'ed_set_minimum_donation_amount', 10, 2 );
--- donation-form/set-minimum-donation-amount.php
+++ donation-form/set-minimum-donation-amount.php
@@ -2,27 +2,12 @@
 /**
  * Set the minimum donation amount required.
  *
- * This function is called when the donation amount is validated, so it needs
- * to return either true or false.
+ * This requires Charitable 1.5+. If you are using a previous version:
+ *
+ * @see https://github.com/Charitable/library/blob/master/donation-form/legacy/set-minimum-donation-amount.php
  */
-function ed_set_minimum_donation_amount( $valid, Charitable_Donation_Form_Interface $donation_form ) {
-
-    /* Replace 2 with your minimum donation amount. */
-    $minimum = 2;
-
-    if ( $donation_form::get_donation_amount() < $minimum ) {
-
-        charitable_get_notices()->add_error( sprintf(
-            __( 'You must donate more than %s.', 'charitable' ),
-            charitable_format_money( $minimum )
-        ) );    
-
-        $valid = false;
-
-    }
-
-    return $valid;
-
+function ed_charitable_set_minimum_donation_amount() {
+    return 2;
 }
 
-add_filter( 'charitable_validate_donation_form_submission_amount_check', 'ed_set_minimum_donation_amount', 10, 2 );
+add_filter( 'charitable_minimum_donation_amount', 'ed_charitable_set_minimum_donation_amount' );

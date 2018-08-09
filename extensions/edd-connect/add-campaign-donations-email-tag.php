10ffa65558bb54eef99427a50ca2a1baf3e0aae9
Eric Daams <eric@164a.com>
2018-08-09 12:13:03 +0930

    Added additional option for displaying campaign donations in EDD emails.

--- extensions/edd-connect/add-campaign-donations-email-tag.php
+++ extensions/edd-connect/add-campaign-donations-email-tag.php
@@ -28,7 +28,7 @@ add_action( 'edd_add_email_tags', 'ed_charitable_edd_add_campaign_donations_emai
 /**
  * Define the output of the tag.
  *
- * @param   int $payemtn_id
+ * @param   int $payment_id
  * @return  string
  */
 function ed_charitable_edd_print_payment_donations( $payment_id ) {
@@ -39,7 +39,7 @@ function ed_charitable_edd_print_payment_donations( $payment_id ) {
     if ( ! $campaign_donations ) {
         return $output;
     }
-  
+
     foreach ( $campaign_donations as $campaign_donation ) {
 
         /** 
@@ -51,6 +51,50 @@ function ed_charitable_edd_print_payment_donations( $payment_id ) {
          * $campaign_donation->amount        = The amount donated.
          */
         $output .= $campaign_donation->campaign_name . ': ' . charitable_format_money( $campaign_donation->amount ) . PHP_EOL;
+
+    }
+  
+    return $output;
+}
+
+/**
+ * As above, this can be used to display the donations made as
+ * part of a payment, but it will bundle together donations to
+ * the same campaign so they just appear once.
+ *
+ * @param   int $payment_id
+ * @return  string
+ */
+function ed_charitable_edd_print_payment_grouped_donations( $payment_id ) {
+    $output = '';
+  
+    $campaign_donations = Charitable_EDD_Payment::get_instance()->get_campaign_donations_through_payment( $payment_id );
+    
+    if ( ! $campaign_donations ) {
+        return $output;
+    }
+
+    $campaigns = array();
+
+    /* Group together campaign donations to the same campaign. */
+    foreach ( $campaign_donations as $campaign_donation ) {
+        if ( ! array_key_exists( $campaign_donation->campaign_id, $campaigns ) ) {
+            $campaigns[ $campaign_donation->campaign_id ] = $campaign_donation;
+        } else {
+            $campaigns[ $campaign_donation->campaign_id ]->amount += $campaign_donation->amount;
+        }
+    }
+
+    foreach ( $campaigns as $campaign ) {        
+        /** 
+         * Variables you can use:
+         *
+         * $campaign->campaign_name = The name of the campaign that received a donation.
+         * $campaign->campaign_id   = The ID of the campaign that received a donation.
+         * $campaign->donation_id   = The ID of the donation.
+         * $campaign->amount        = The total amount donated to the campaign.
+         */
+        $output .= $campaign->campaign_name . ': ' . charitable_format_money( $campaign->amount ) . PHP_EOL;
 
     }
   

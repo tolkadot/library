2a6f9eee0af4329556a04d9c96cfc9e676a7718e
Eric Daams <eric@164a.com>
2019-05-24 15:48:22 +0930

    Change custom amount field label for recurring donations.

--- donation-form/change-custom-amount-field-label.php
+++ donation-form/change-custom-amount-field-label.php
@@ -10,3 +10,6 @@ function ed_charitable_donation_form_set_custom_amount_field_text( $label ) {
 }
 
 add_filter( 'charitable_donation_amount_form_custom_amount_text', 'ed_charitable_donation_form_set_custom_amount_field_text' );
+
+/* To use the same text for the Recurring Donations custom amount text, include the following line. */
+add_filter( 'charitable_recurring_donation_amount_form_custom_amount_text', 'ed_charitable_donation_form_set_custom_amount_field_text' );

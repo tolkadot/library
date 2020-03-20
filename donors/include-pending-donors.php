9ab618fe344758ac752778e298e831fc06ed8d61
Eric Daams <eric@164a.com>
2020-03-21 08:37:27 +0930

    Made it work for shortcodes too

--- donors/include-pending-donors.php
+++ donors/include-pending-donors.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * By default, the Donors widget will only include donors with
+ * By default, the Donors widget & shortcode will only include donors with
  * Paid donations. With this snippet, you can make sure that
  * donors with Pending donations are also included.
  */
-add_filter(
-	'charitable_donors_widget_donor_query_args',
-	function( $query_args ) {
-		$query_args['status'] = [ 'charitable-pending', 'charitable-completed', 'charitable-preapproved' ];
-		return $query_args;
-	}
-);
+function ed_include_pending_donors_in_donors_widget_shortcode( $query_args ) {
+	$query_args['status'] = [ 'charitable-pending', 'charitable-completed', 'charitable-preapproved' ];
+	return $query_args;
+}
+
+add_filter( 'charitable_donors_widget_donor_query_args', 'ed_include_pending_donors_in_donors_widget_shortcode' );
+add_filter( 'charitable_donors_shortcode_donor_query_args', 'ed_include_pending_donors_in_donors_widget_shortcode' );

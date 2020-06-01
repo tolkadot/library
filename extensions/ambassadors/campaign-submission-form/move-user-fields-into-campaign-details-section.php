c47c392b3c352e5a741862264fa763f0e6a45633
Eric Daams <eric@164a.com>
2020-06-01 13:16:23 +0930

    Tidied up snippet

--- extensions/ambassadors/campaign-submission-form/move-user-fields-into-campaign-details-section.php
+++ extensions/ambassadors/campaign-submission-form/move-user-fields-into-campaign-details-section.php
@@ -5,29 +5,37 @@
  * @param  array $fields All of the campaign form fields.
  * @return array
  */
-add_filter( 'charitable_campaign_submission_fields', function( $fields ) {
-	// To remove any fields from the form altogether, remove them from this list.
-	$moved = [
-		'first_name',
-		'last_name',
-		'user_email',
-		'city',
-		'state',
-		'country',
-		'user_description',
-		'organisation',
-	];
+add_filter(
+	'charitable_campaign_submission_fields',
+	function( $fields ) {
+		// To remove any fields from the form altogether, remove them from this list.
+		$moved = [
+			'first_name',
+			'last_name',
+			'user_email',
+			'city',
+			'state',
+			'country',
+			'user_description',
+			'organisation',
+		];
 
-	foreach ( $moved as $key ) {
-		if ( ! array_key_exists( $key, $fields['user_fields']['fields'] ) ) {
-			continue;
+		foreach ( $moved as $key ) {
+			if ( ! array_key_exists( $key, $fields['user_fields']['fields'] ) ) {
+				continue;
+			}
+
+			$fields['campaign_fields']['fields'][ $key ] = $fields['user_fields']['fields'][ $key ];
 		}
 
-		$fields['user_fields']['fields'][ $key ] = $fields['campaign_fields']['fields'][ $key ];
+		// Finally, remove the "Your Details" section.
+		unset( $fields['user_fields'] );
+
+		// Sort the fields.
+		$campaign_fields = $fields['campaign_fields']['fields'];
+		uasort( $campaign_fields, 'charitable_priority_sort' );
+		$fields['campaign_fields']['fields'] = $campaign_fields;
+
+		return $fields;
 	}
-
-	// Finally, remove the "Your Details" section.
-    unset( $fields['user_fields'] );
-
-    return $fields;
-} );
+);

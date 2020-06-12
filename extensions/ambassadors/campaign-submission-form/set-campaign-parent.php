c84a86322e65b13af06a8c620d20081503c3683b
Eric Daams <eric@164a.com>
2020-06-12 16:46:44 +0930

    Updated example for Ambassadors 2

--- extensions/ambassadors/campaign-submission-form/set-campaign-parent.php
+++ extensions/ambassadors/campaign-submission-form/set-campaign-parent.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * As of Ambassadors 2.0, you may not need this snippet anymore. Instead,
+ * you can specify a parent for the campaign form with the 'parent_id'
+ * parameter:
+ *
+ * [charitable_submit_campaign parent_id=1188]
+ *
+ * However, this does require a few changes:
+ *
+ * 1. Under Charitable > Settings > Ambassadors, "Fundraisers for existing campaigns"
+ *    should be enabled.
+ * 2. The parent campaign you choose (ID 1188 in this example), needs to have
+ *    peer-to-peer fundraising enabled.
+ * 3. The fundraiser submission form is designed to be a little simpler than the regular
+ *    campaign form, with less fields and the ability to set defaults for certain fields.
+ */
 
 /**
  * This example shows how to set the parent of every campaign that is submitted.
@@ -6,11 +22,20 @@
  * @param   array $values
  * @return  array $values
  */
-function ed_set_submitted_campaign_parent( $values ) {
-
+add_filter( 'charitable_campaign_submission_core_data', function( $values ) {
 	$values['post_parent'] = 1188; // Replace 1188 with the ID of your parent campaign.
-
 	return $values;
-}
+});
 
-add_filter( 'charitable_campaign_submission_core_data', 'ed_set_submitted_campaign_parent' );
+/**
+ * Ensure the post parent field is saved.
+ *
+ * This is required in Ambassadors 2+.
+ *
+ * @param  array $fields The map of fields to be saved.
+ * @return array
+ */
+add_filter( 'charitable_campaign_submission_fields_map', function( $fields ) {
+	$fields['core']['post_parent'] = 'number';
+	return $fields;
+});

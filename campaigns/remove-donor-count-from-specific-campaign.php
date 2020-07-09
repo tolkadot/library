960f1873153e8fc464aa34b72b0d80e588ad9362
Eric Daams <eric@164a.com>
2020-07-09 14:28:25 +0930

    Modified snippet slightly to allow use for more than one campaign.

--- campaigns/remove-donor-count-from-specific-campaign.php
+++ campaigns/remove-donor-count-from-specific-campaign.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 /**
  * Hide the campaign donor count from a specific campaign, using the campaign ID.
  *
  * @param   Charitable_Campaign $campaign
  */
 function charitable_template_campaign_donor_count( $campaign ) {
+    // Remove stats from campaigns with IDs of 6 and 22.
+    $campaigns = [ 6, 22 ];
 
-    $campaign_id = 6; // Replace with the ID of your campaign.
-    
-    if ( $campaign_id == $campaign->ID ) {
+    if ( in_array( $campaign_id, $campaigns ) ) {
         return;
     }
 

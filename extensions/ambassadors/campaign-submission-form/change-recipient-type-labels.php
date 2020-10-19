48358caffe6d77c274b9b25eadc07878a6c4e2f5
Eric Daams <eric@164a.com>
2020-10-19 10:39:37 +0930

    Added snippet for changing the fundraiser recipient type labels.

--- extensions/ambassadors/campaign-submission-form/change-recipient-type-labels.php
+++ extensions/ambassadors/campaign-submission-form/change-recipient-type-labels.php
@@ -18,9 +18,9 @@
  * In this example we're changing the "Personal Causes"  recipient type.
  */
 function ed_charitable_change_personal_recipient_type_details( $details ) {
-    $details['label'] = 'Your Preferred Label for Personal Causes';
-    $details['description'] = 'Your preferred description for Personal Causes';
-    return $details;
+	$details['label']       = 'Your Preferred Label for Personal Causes';
+	$details['description'] = 'Your preferred description for Personal Causes';
+	return $details;
 }
 
 add_filter( 'charitable_ambassadors_personal_recipient_type', 'ed_charitable_change_personal_recipient_type_details' );
@@ -29,9 +29,20 @@ add_filter( 'charitable_ambassadors_personal_recipient_type', 'ed_charitable_cha
  * In this example we're changing the "Your Organization"  recipient type.
  */
 function ed_charitable_change_ambassador_recipient_type_details( $details ) {
-    $details['label'] = 'Your Preferred Label for Your Organization';
-    $details['description'] = 'Your preferred description for Your Organization';
-    return $details;
+	$details['label']       = 'Your Preferred Label for Your Organization';
+	$details['description'] = 'Your preferred description for Your Organization';
+	return $details;
 }
 
 add_filter( 'charitable_ambassadors_ambassador_recipient_type', 'ed_charitable_change_ambassador_recipient_type_details' );
+
+/**
+ * In this example we're changing the "Fundraisers for existing campaigns"  recipient type.
+ */
+function ed_charitable_change_ambassador_recipient_type_details( $details ) {
+	$details['label']       = 'Your Preferred Label for Another Campaign';
+	$details['description'] = 'Your preferred description for Another Campaign';
+	return $details;
+}
+
+add_filter( 'charitable_ambassadors_fundraiser_recipient_type', 'ed_charitable_change_ambassador_recipient_type_details' );
